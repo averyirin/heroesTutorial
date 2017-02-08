2dc7f2b0caf3eb513ad203ecb37564edda694e56
Irin <averyirin@gmail.com>
2017-02-08 14:57:30 -0500

    Added cat logo, and catified site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,15 @@ import { Component }          from '@angular/core';
   moduleId: module.id,
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
+    <h1>{{title}}<img src="./app/images/catlogo.png"/></h1>
     <nav>
       <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="/heroes" routerLinkActive="active">Cats</a>
     </nav>
     <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Tour of Cats';
 }
